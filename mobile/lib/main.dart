9d9d593b3abe78bb9c98bfd12eb4be7a1b66c7dc
omerkoc <ofk6409@gmail.com>
2026-05-10 00:08:06 +0300

    fix

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'core/auth/auth_provider.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('tr', null);
   await GoogleSignIn.instance.initialize();
   runApp(const ProviderScope(child: CaizMiApp()));
 }
