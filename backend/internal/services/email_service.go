7a29ff8238c0960c77a8318e2753d00a07ea6117
omerkoc <ofk6409@gmail.com>
2026-05-09 17:29:39 +0300

    feat: add smtp email service

---
+++ backend/internal/services/email_service.go
@@ -0,0 +1,83 @@
+package services
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/smtp"
+	"strings"
+)
+
+type EmailService interface {
+	Send(to, subject, htmlBody string) error
+}
+
+type SMTPEmailService struct {
+	host     string
+	port     string
+	user     string
+	password string
+	from     string
+}
+
+func NewSMTPEmailService(host, port, user, password, from string) *SMTPEmailService {
+	return &SMTPEmailService{host: host, port: port, user: user, password: password, from: from}
+}
+
+func (s *SMTPEmailService) Send(to, subject, htmlBody string) error {
+	auth := smtp.PlainAuth("", s.user, s.password, s.host)
+
+	headers := strings.Join([]string{
+		"From: " + s.from,
+		"To: " + to,
+		"Subject: " + subject,
+		"MIME-Version: 1.0",
+		"Content-Type: text/html; charset=UTF-8",
+	}, "\r\n")
+	msg := headers + "\r\n\r\n" + htmlBody
+
+	addr := net.JoinHostPort(s.host, s.port)
+
+	tlsConfig := &tls.Config{ServerName: s.host}
+	conn, err := tls.Dial("tcp", addr, tlsConfig)
+	if err != nil {
+		// TLS başarısız olursa STARTTLS dene
+		return smtp.SendMail(addr, auth, s.user, []string{to}, []byte(msg))
+	}
+	defer conn.Close()
+
+	client, err := smtp.NewClient(conn, s.host)
+	if err != nil {
+		return fmt.Errorf("smtp client oluşturulamadı: %w", err)
+	}
+	defer client.Close()
+
+	if err = client.Auth(auth); err != nil {
+		return fmt.Errorf("smtp auth başarısız: %w", err)
+	}
+	if err = client.Mail(s.user); err != nil {
+		return err
+	}
+	if err = client.Rcpt(to); err != nil {
+		return err
+	}
+	w, err := client.Data()
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprint(w, msg)
+	if err != nil {
+		return err
+	}
+	return w.Close()
+}
+
+// NoopEmailService — test ve geliştirme ortamı için email göndermeden loglar.
+type NoopEmailService struct{}
+
+func NewNoopEmailService() *NoopEmailService { return &NoopEmailService{} }
+
+func (s *NoopEmailService) Send(to, subject, _ string) error {
+	fmt.Printf("[EMAIL NOOP] To: %s | Subject: %s\n", to, subject)
+	return nil
+}
